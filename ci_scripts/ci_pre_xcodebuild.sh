8d0183cd916e62361e34d6af991178154c8799f3
Saroj Lakum <saroj.lakum@tristatetechnology.co.in>
2023-08-17 15:03:25 +0530

    add pre and post script

---
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+#  ci_pre_xcodebuild.sh
+#  GitHubCICD
+#
+#  Created by tristate22 on 17.08.23.
+#  
+ echo "🧩 Stage: PRE-Xcode Build is activated .... "
+
+ # You can add additional scripts here...
+
+ echo "🎯 Stage: PRE-Xcode Build is DONE .... "
+
+ exit 0
