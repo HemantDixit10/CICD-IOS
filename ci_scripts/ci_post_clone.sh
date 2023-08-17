2dc895370fa256f6a0680c4d976dc059199b0c92
Saroj Lakum <saroj.lakum@tristatetechnology.co.in>
2023-08-17 14:46:30 +0530

    add script

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+#  ci_post_clone.sh
+#  GitHubCICD
+#
+#  Created by tristate22 on 17.08.23.
+#  
+
+# Install CocoaPods using Homebrew.
+
+brew install cocoapods
+
+# Install dependencies you manage with CocoaPods.
+
+pod install
