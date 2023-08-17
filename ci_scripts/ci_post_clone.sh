8a03e8ac830d132981189a6702cb95b2aadfcca6
HemantDixit10 <hemant.dixit@tristatetechnology.com>
2023-08-17 18:33:42 +0530

    Updated Post Clone build for Install Pod

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -8,18 +8,9 @@
 cd ..
 
 echo ">>> SETUP ENVIRONMENT"
-echo 'export GEM_HOME=$HOME/gems' >>~/.bash_profile
-echo 'export PATH=$HOME/gems/bin:$PATH' >>~/.bash_profile
-export GEM_HOME=$HOME/gems
-export PATH="$GEM_HOME/bin:$PATH"
+#!/bin/sh
 
-echo ">>> INSTALL BUNDLER"
-gem install bundler --install-dir $GEM_HOME
+brew install cocoapods
+echo ">>> install Pods"
 
-echo ">>> INSTALL DEPENDENCIES"
-bundle install
-
-gem update --system 3.2.3
-
-echo ">>> INSTALL PODS"
-bundle exec pod install
+pod install
