3de4083dace66f3f8b093e0f3e25440262067fdf
Saroj Lakum <saroj.lakum@tristatetechnology.co.in>
2023-08-17 15:36:19 +0530

    6

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -6,8 +6,19 @@
 #  Created by tristate22 on 17.08.23.
 #  
 
-# Install CocoaPods using Homebrew.
-sudo gem install cocoapods
+cd ..
 
-# Install dependencies you manage with CocoaPods.
-pod install
+echo ">>> SETUP ENVIRONMENT"
+echo 'export GEM_HOME=$HOME/gems' >>~/.bash_profile
+echo 'export PATH=$HOME/gems/bin:$PATH' >>~/.bash_profile
+export GEM_HOME=$HOME/gems
+export PATH="$GEM_HOME/bin:$PATH"
+
+echo ">>> INSTALL BUNDLER"
+gem install bundler --install-dir $GEM_HOME
+
+echo ">>> INSTALL DEPENDENCIES"
+bundle install
+
+echo ">>> INSTALL PODS"
+bundle exec pod install
